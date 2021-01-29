c219ffbbb6d403dae58f1fc60d2edf06d105706a
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-29 20:56:21 +0700

    [-] rm : clg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
         )
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
                 setProfile(data);
                 setLoading(false);
             });
